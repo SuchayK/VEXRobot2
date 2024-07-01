8d683d1a291d2c530234732bf32065d631d12219
SuchayK <67595987+SuchayK@users.noreply.github.com>
2024-07-01 16:16:59 -0500

    Odometry + Kc

--- main.cpp
+++ main.cpp
@@ -58,27 +58,35 @@ void leftDegrees(double x) {
   right_drive.spinFor(fwd, x, degrees);
 }
 
-void moveForwardPID(double targetDistance) {
-  double kP = 0.6;
-  double kI = 0.04;
-  double kD = 0.3;
+void moveForward(double targetDistance) {
+  double kP = 0.08;
+  double kI = 0.01;
+  double kD = 0.0075;
+  double Kc = 0.5;
   double error = targetDistance;
   double previousError = 0;
   double integral = 0;
   double derivative;
   double motorPower;
 
+  double initialAngle = inertial.rotation(degrees);
+  double currentAngle;
+  double angleError;
+
   left_drive.resetPosition();
   right_drive.resetPosition();
 
   while (fabs(error) > 0.1) {
+    currentAngle = inertial.rotation(degrees);
+    angleError = initialAngle - currentAngle;
+
     error = targetDistance - left_drive.position(turns);
     integral += error;
     derivative = error - previousError;
     motorPower = (kP * error) + (kI * integral) + (kD * derivative);
 
-    left_drive.spin(fwd, motorPower, pct);
-    right_drive.spin(fwd, motorPower, pct);
+    left_drive.spin(fwd, motorPower + Kc * angleError, pct);
+    right_drive.spin(fwd, motorPower - Kc * angleError, pct);
 
     previousError = error;
     wait(20, msec);
@@ -86,28 +94,39 @@ void moveForwardPID(double targetDistance) {
 
   left_drive.stop();
   right_drive.stop();
+  updateOdometry();
+
 }
 
-void leftPID(double targetDegrees) {
-  double kP = 0.6;
-  double kI = 0.04;
-  double kD = 0.3;
-  double error = -targetDegrees;
+void moveBack(double targetDistance) {
+  double kP = 0.08;
+  double kI = 0.01;
+  double kD = 0.0075;
+  double Kc = 0.5;
+  double error = targetDistance;
   double previousError = 0;
   double integral = 0;
   double derivative;
   double motorPower;
 
-  inertial.resetRotation();
+  double initialAngle = inertial.rotation(degrees);
+  double currentAngle;
+  double angleError;
 
-  while (fabs(error) > 1) {
-    error = targetDegrees - inertial.rotation(degrees);
+  left_drive.resetPosition();
+  right_drive.resetPosition();
+
+  while (fabs(error) > 0.1) {
+    currentAngle = inertial.rotation(degrees);
+    angleError = initialAngle - currentAngle;
+
+    error = targetDistance - left_drive.position(turns);
     integral += error;
     derivative = error - previousError;
     motorPower = (kP * error) + (kI * integral) + (kD * derivative);
 
-    left_drive.spin(fwd, motorPower, pct);
-    right_drive.spin(reverse, motorPower, pct);
+    left_drive.spin(reverse, motorPower + Kc * angleError, pct);
+    right_drive.spin(reverse, motorPower - Kc * angleError, pct);
 
     previousError = error;
     wait(20, msec);
@@ -115,12 +134,14 @@ void leftPID(double targetDegrees) {
 
   left_drive.stop();
   right_drive.stop();
+  updateOdometry();
+
 }
 
-void rightPID(double targetDegrees) {
-  double kP = 0.6;
-  double kI = 0.04;
-  double kD = 0.3;
+void turnPID(double targetDegrees) {
+  double kP = 0.08;
+  double kI = 0.01;
+  double kD = 0.0075;
   double error = targetDegrees;
   double previousError = 0;
   double integral = 0;
@@ -144,6 +165,7 @@ void rightPID(double targetDegrees) {
 
   left_drive.stop();
   right_drive.stop();
+  updateOdometry();
 }
 
 void setLeftWing(bool state) {
@@ -152,6 +174,51 @@ void setLeftWing(bool state) {
 
 void setRightWing(bool state) {
   wings.right_wing.set(state);
+}
+
+void updateOdometry() {
+
+  static double lastLeftEncoder = 0.0;
+  static double lastRightEncoder = 0.0;
+  static double lastBackEncoder = 0.0;
+
+  double currentLeftEncoder = leftEncoder.position(degrees);
+  double currentRightEncoder = rightEncoder.position(degrees);
+  double currentBackEncoder = backEncoder.position(degrees);
+
+  double deltaLeft = currentLeftEncoder - lastLeftEncoder;
+  double deltaRight = currentRightEncoder - lastRightEncoder;
+  double deltaBack = currentBackEncoder - lastBackEncoder;
+
+  lastLeftEncoder = currentLeftEncoder;
+  lastRightEncoder = currentRightEncoder;
+  lastBackEncoder = currentBackEncoder;
+
+  double deltaLeftInches = (deltaLeft / tpr) * wheelDiameter * M_PI;
+  double deltaRightInches = (deltaRight / tpr) * wheelDiameter * M_PI;
+  double deltaBackInches = (deltaBack / tpr) * wheelDiameter * M_PI;
+
+  double deltaTheta = (deltaRightInches - deltaLeftInches) / wheelBaseWidth;
+  double deltaX = (deltaLeftInches + deltaRightInches) / 2.0;
+  double deltaY = deltaBackInches - (deltaTheta * backWheelOffset);
+
+  if(deltaTheta > 180)
+			deltaTheta = deltaTheta - 360;
+		if(deltaTheta < -180)
+			deltaTheta = 360 + deltaTheta;
+
+  robotTheta += deltaTheta;
+  robotX += deltaX * cos(robotTheta) - deltaY * sin(robotTheta);
+  robotY += deltaX * sin(robotTheta) + deltaY * cos(robotTheta);
+
+}
+
+float to_rad(float angle_deg){
+  return(angle_deg/(180.0/M_PI));
+}
+
+float to_deg(float angle_rad){
+  return(angle_rad*(180.0/M_PI));
 }
 
 void flywheelRampUp(double targetSpeed, double rampTime) {
